4edb8c3a3b328fdabd0e1a15b88c85d6983e1305
Abdulsamad1109 <gbadebosamad0@gmail.com>
2023-04-18 11:03:46 +0100

    second commit

---
+++ main.js
@@ -0,0 +1,49 @@
+var display = document.getElementById("input");
+console.log(display)
+
+
+function press(e){
+    display.value += e;
+    // document.getElementById("input").value +=e
+    
+}
+
+
+function clearAll(){
+    display.value = ""
+    document.getElementById("ad").innerText = ""
+}
+
+
+function del(){
+    display.value = display.value.slice(0, -1)
+}
+
+
+function equalto(){
+    document.getElementById("ad").innerText = eval(display.value)
+}
+
+
+function percent(){
+    display.value = display/100
+}
+
+function sqr(){
+    display.value = Math.sqrt(display.value)
+}
+
+function raiseTpow(){
+    display.value = Math.pow(display.input, display.input)
+}
+
+
+function normal(){
+    document.getElementById("sci-btn").style.display = "none"
+}
+
+
+function scientific(){
+    document.getElementById("sci-btn").style.display = "block"
+}
+
